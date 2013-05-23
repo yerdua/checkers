e1cac3093bbdc0262d824e26dc5d4712a03f17b3
Audrey Penven <audrey.penven@gmail.com>
2013-05-23 14:38:20 -0700

    Added in move logic and errors

--- board.rb
+++ board.rb
@@ -13,11 +13,24 @@ class Board
     #the dark squares are defined by the sum of their
     #row and column indexes being odd. ex: 0,0 is light, 0,1 is dark
     #pieces can only be placed on dark squares
-    if (row + col).odd?
+    if dark_square?([row, col])
       @grid[row][col] = piece
     else
       raise ArgumentError.new("Can't place pieces on light squares")
     end
+  end
+  
+  def in_board?(location)
+    location.all? {|coord| coord.between?(0,9)}
+  end
+  
+  def dark_square?(location)
+    in_board?(location) && (location[0] + location[1]).odd?
+  end
+  
+  def square_contents(location)
+    contents = self[*location]
+    return contents.color if contents
   end
   
   def display
@@ -41,8 +54,9 @@ class Board
     #black pieces go on dark squares of first 3 rows
     (0..2).each do |row|
       (0..9).each do |col|
-        if (row + col).odd?
-          self[row, col] = Pawn.new(:black, [row, col])
+        loc = [row, col]
+        if dark_square?(loc)
+          self[*loc] = Pawn.new(:black, loc, self)
         end
       end
     end
@@ -50,8 +64,9 @@ class Board
     #white pieces go on dark squares of the last 3 rows
     (7..9).each do |row|
       (0..9).each do |col|
-        if (row + col).odd?
-          self[row, col] = Pawn.new(:white, [row, col])
+        loc = [row, col]
+        if dark_square?(loc)
+          self[*loc] = Pawn.new(:white, loc, self)
         end
       end
     end
--- piece.rb
+++ piece.rb
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+require 'set'
+require 'debugger'
+require_relative 'exceptions'
 
 class Piece
   DELTAS = {
@@ -9,18 +12,70 @@ class Piece
   }
   
   attr_accessor :location
+  attr_reader :color
+  
   def initialize(color, location, board)
     @color = color
+    @location = location
+    @board = board
+    self
   end
   
   def inspect
     @color
   end
   
-  def move(destination)
-    unless (destination[0] + destination[1]).odd?
-      raise ArgumentError.new("C")
+  def jump(direction)
+    jump_space_coords = add_offset(@location, direction)
+    jump_space = @board[*jump_space_coords]
+    destination = add_offset(jump_space_coords, direction)
+    
+    if can_jump?(direction, jump_space, destination)
+      @board[*@location] = nil
+      @board[*jump_space_coords] = nil
+      @board[*destination] = self
+      @location = destination
+    else
+      raise InvalidMoveError.new("Can't jump #{direction} to #{destination}")
     end
+  end
+  
+  def move(direction)
+    destination = add_offset(@location, direction)
+    
+    if can_move?(direction, destination)
+      @board[*@location] = nil
+      @board[*destination] = self
+      @location = destination
+    else
+      raise InvalidMoveError.new("Can't move #{direction} to #{destination}")
+    end
+  end
+  
+  def can_move?(direction, destination)
+    valid_direction?(direction) && valid_destination?(destination)
+  end
+  
+  def can_jump?(direction, jump_space, destination)
+    valid_direction?(direction) && valid_destination?(destination) &&
+      !jump_space.nil? && (jump_space.color == opponent)
+  end
+  
+  def valid_destination?(destination)
+    @board.dark_square?(destination) && @board[*destination].nil?
+  end
+  
+  def valid_direction?(direction)
+    @directions.include? direction
+  end
+  
+  def add_offset(location, direction)
+    offset = DELTAS[direction]
+    [offset[0] + location[0], offset[1] + location[1]]
+  end
+  
+  def opponent
+    @color == :white ? :black : :white
   end
 
 end
@@ -31,7 +86,7 @@ class Pawn < Piece
     :black => '⚫'
   }
   
-  attr_reader @directions
+  attr_reader :directions
   
   def initialize(color, location, board)
     super(color, location, board)
