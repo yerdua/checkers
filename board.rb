aa04c9dbadd8920557bbb11c73743fc4f83e6bc6
Audrey Penven <audrey.penven@gmail.com>
2013-05-23 10:40:46 -0700

    added method to place pieces in initial positions. Created subclasses of pieces: king and pawn'

--- board.rb
+++ board.rb
@@ -20,15 +20,38 @@ class Board
   
   def display
     @grid.each do |row|
-      r = row.map(&:to_s).join(' | ')
-      puts r
+      r = row.map do |sq|
+        sq.to_s.rjust(3).ljust(6)
+      end
+      puts r.join("|")
+      puts "-------" * 10
     end
     nil
   end
   
-  protected
+  # protected
   
   def make_empty_grid
     @grid = Array.new(10) { Array.new(10)}
+  end
+  
+  def place_pieces
+    #black pieces go on dark squares of first 3 rows
+    (0..2).each do |row_index|
+      (0..9).each do |col_index|
+        if (row_index + col_index).odd?
+          self[row_index, col_index] = Piece.new(:black)
+        end
+      end
+    end
+    
+    #white pieces go on dark squares of the last 3 rows
+    (7..9).each do |row_index|
+      (0..9).each do |col_index|
+        if (row_index + col_index).odd?
+          self[row_index, col_index] = Piece.new(:white)
+        end
+      end
+    end
   end
 end
--- piece.rb
+++ piece.rb
@@ -1,14 +1,29 @@
 # -*- coding: utf-8 -*-
 
 class Piece
+  attr_accessor :location
+  def initialize(color, location)
+    @color = color
+  end
+
+end
+
+class Pawn < Piece
   SYMBOLS = {
     :white => '⚪',
     :black => '⚫'
   }
   
-  def initialize(color)
-    @color = color
+  def to_s
+    SYMBOLS[@color]
   end
+end
+
+class King < Piece
+  SYMBOLS = {
+    :white => '⚪K',
+    :black => '⚫K'
+  }
   
   def to_s
     SYMBOLS[@color]
